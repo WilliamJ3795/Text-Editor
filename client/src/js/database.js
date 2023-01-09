18636709fd0a705aca58145b4d415493726ce833
William James <Williamj3795@gmail.com>
2023-01-08 20:48:51 -0500

    file change

---
+++ client/src/js/database.js
@@ -0,0 +1,38 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+  
+export const putDb = async (content) => {
+
+  const jateDb = await openDB('jate', 1);
+  const dbRead = jateDb.transaction('jate', 'readwrite');
+  const store = dbRead.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+
+  const result = await request;
+  console.log ('data has been saved to DB', result);
+  return request;
+}
+
+export const getDb = async () => {
+
+  const jateDb = await openDB('jate', 1);
+  const dbRead = jateDb.transaction('jate', 'readonly');
+  const store = dbRead.objectStore('jate');
+  const request = store.getAll();
+  
+  const result = await request;
+  console.log ('result.value', result);
+  return request?.value;
+}
+initdb();
